9f057eb65b3a821b9d1f9ff4c25ec5f979aed715
riosdrc <riosdrc@gmail.com>
2023-12-11 01:59:35 -0600

    Adding loading page

--- app/[cat]/CatContainer.jsx
+++ app/[cat]/CatContainer.jsx
@@ -4,12 +4,16 @@ import { useEffect, useState } from 'react'
 import BlogList from '../components/BlogList'
 import axios from 'axios'
 import API_BASE_URL from '../apiConfig'
+import Loading from '../loading'
 
 export default function CatContainer({cat}) {
     const [blogs, setblogs] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    
     async function getBlogs() {
         try {
           const res = await axios(API_BASE_URL + '/posts/?cat=' + cat)
+          setIsLoading(false)
           setblogs(res.data)
         } catch (error) {
           console.log(error)
@@ -20,7 +24,12 @@ export default function CatContainer({cat}) {
       getBlogs();
     }, [])
 
-  return (
+  return (<>
+  {isLoading ?
+    <Loading />
+    :
     <BlogList blogs={blogs}/>
+  }
+  </>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,17 @@ import { useEffect, useState } from 'react'
 import API_BASE_URL from './apiConfig'
 import BlogList from './components/BlogList'
 import axios from 'axios'
+import Loading from './loading'
 
 
 export default function Home() {
   const [blogs, setBlogs] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   
   async function getBlogs() {
     try {
       const res = await axios(API_BASE_URL + '/posts')
+      setIsLoading(false)
       setBlogs(res.data)
     } catch (error) {
       console.log(error)
@@ -22,7 +25,11 @@ export default function Home() {
   }, [])
   return (
     <main className='mx-[15%] md:mx-[5%] mt-8 mb-14'>      
-      <BlogList blogs={blogs} />
+      { !isLoading ?
+        <BlogList blogs={blogs} />
+      :
+        <Loading />
+      }
     </main>
   )
 }
