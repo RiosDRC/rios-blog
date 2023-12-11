e6a3ce6c8644dfe60afa90ecfde8974afcf32894
riosdrc <riosdrc@gmail.com>
2023-12-11 00:51:59 -0600

    Home: Use client

--- app/components/BlogList.jsx
+++ app/components/BlogList.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import DOMPurify from "isomorphic-dompurify"
 import Image from 'next/image'
 import Link from 'next/link'
--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,25 @@
+"use client"
+import { useEffect, useState } from 'react'
 import API_BASE_URL from './apiConfig'
 import BlogList from './components/BlogList'
 import axios from 'axios'
 
-async function getBlogs() {
-  try {
-    const res = await fetch(API_BASE_URL + '/posts', {
-      next: {
-        revalidate: 0
-      }
-    })
-    return res.json()
-  } catch (error) {
-    console.log(error)
-  }
-}
 
-export default async function Home() {
-  const blogs = await getBlogs();
+export default function Home() {
+  const [blogs, setBlogs] = useState([])
+  
+  async function getBlogs() {
+    try {
+      const res = await axios(API_BASE_URL + '/posts')
+      setBlogs(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getBlogs();
+  }, [])
   return (
     <main className='mx-[15%] md:mx-[5%] mt-8 mb-14'>      
       <BlogList blogs={blogs} />
